d38d3e0cd055223a027db356bade612573740fb6
kimwoolina <rladnfls0500@gmail.com>
2024-08-29 22:06:49 +0900

    feat: accounts features, refact: update template directory on settings.py

--- llm_pjt/accounts/models.py
+++ llm_pjt/accounts/models.py
@@ -4,4 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+
+    # 프로필 이미지
+    profile_image = models.ImageField(
+        upload_to='images/profile_images/',  # 프로필 이미지가 저장될 디렉토리
+        null=True,  # 이미지가 없어도 허용
+        blank=True,  # 관리자 폼에서 필수 입력이 아님
+        default='images/default/default_profile_image.png'  # 기본 이미지의 경로
+    )
