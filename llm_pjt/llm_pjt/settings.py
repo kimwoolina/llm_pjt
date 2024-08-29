4ec9aaac0d9f9b77e6b793987a1f1536111931f6
kimwoolina <rladnfls0500@gmail.com>
2024-08-29 20:45:54 +0900

    style: write index.html

--- llm_pjt/llm_pjt/settings.py
+++ llm_pjt/llm_pjt/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions'
+    'django_extensions',
     'accounts',
-    'myapp'
+    'myapp',
 
 ]
 
@@ -125,6 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- llm_pjt/llm_pjt/urls.py
+++ llm_pjt/llm_pjt/urls.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.urls import path, re_path
+from django.views.generic.base import RedirectView
+
+from myapp import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^$', RedirectView.as_view(url='/index/', permanent=True)),
+    path("index/", views.index, name="index"),
+    path("accounts/", include("accounts.urls")),
+    path("myapp/", include("myapp.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
